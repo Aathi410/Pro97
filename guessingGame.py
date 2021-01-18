fb62dfcdd2f50fa889036a67ebd680d0e0423d81
Aathi410 <n4aathi04@gmail.com>
2021-01-18 17:47:50 +0530

    commit1

---
+++ guessingGame.py
@@ -0,0 +1,26 @@
+import random;
+
+print("Guessing Game ")
+print("Guess A Number Between 1 To 9")
+
+chances = 0
+
+number = random.randint(1,9)
+
+while chances  <5 :
+    guess = int(input("Enter The Guessed Number : "))
+
+    if guess == number  : 
+        print("Congratulation You Won The Game :)")
+        break
+
+    elif guess < number:
+        print("Your Guess Was Too Low. Guess A Number Higher Than", guess)
+
+    else:
+        print("Your Guess Was Too High. Guess A Number Lower Than", guess)
+
+    chances += 1
+
+    if not chances < 5 :
+        print("You Lose !!! The Number Is" , number,"Try Again :)")
